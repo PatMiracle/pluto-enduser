259b8af52b503580a25839e5696981e61f94782f
Miracle <pimiracledev@gmail.com>
2025-11-25 04:03:13 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
 "use client";
 import { useUserQuery } from "@/services/user-api";
 import useAuthStore from "@/store/AuthStore";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { useRouter } from "next/router";
 
 export default function Home() {
-  const { isPending, isError, error } = useUserQuery();
-
-  if (isPending) {
-    return (
-      <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
-        <div className="border-green-normal h-14 w-14 animate-spin rounded-full border-0 border-y-2"></div>
-        <p>Loading Application...</p>
-      </div>
-    );
-  }
+  const { isError, data: user } = useUserQuery();
+  const { logout } = useAuthStore();
 
   if (isError) {
-    redirect("/login");
+    logout().then(() => redirect("/login"));
   }
 
-  return <Link href="/login">Login</Link>;
+  if (user) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <div className="border-green-normal h-14 w-14 animate-spin rounded-full border-0 border-y-2"></div>
+      <p>Loading Application...</p>
+    </div>
+  );
 }
