ee42b3dc3b4662f5bbe408096f1293d269551a4b
Miracle <pimiracledev@gmail.com>
2025-11-25 04:20:07 +0100

    wip: create new password

---
+++ app/(auth)/create-new-password/CreateNewPasswordForm.tsx
@@ -0,0 +1,16 @@
+import AuthLayout from "@/components/auth/AuthLayout";
+
+export default function CreateNewPasswordForm() {
+  return (
+    <AuthLayout
+      title="Create New Password"
+      sideTitle="Don’t have an Account?"
+      sideSubtitle="Enter your personal details to start with us today"
+      sideButtonLabel="Register Me"
+      sideButtonHref="/signup"
+      reverse
+    >
+      <CreateNewPasswordForm />
+    </AuthLayout>
+  );
+}
