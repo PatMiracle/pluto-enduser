5bc795d8eefea50f165c53ffb7b8f89981db4533
Miracle <pimiracledev@gmail.com>
2025-11-25 04:07:49 +0100

    fix logout route

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,12 +2,15 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    const response = NextResponse.json({}).cookies.delete("refreshToken");
+    const response = NextResponse.json({ success: true });
+
+    response.cookies.delete("refreshToken");
+
     return response;
   } catch (error: any) {
     return NextResponse.json(
       { message: "Sorry there was an error" },
-      { status: error.status },
+      { status: error?.status || 500 },
     );
   }
 }
