a529a3aa7113ebe9dd7444f025c45fdf977c18a7
Miracle <pimiracledev@gmail.com>
2025-11-28 01:01:50 +0100

    responsive

--- app/(main)/requests/page.tsx
+++ app/(main)/requests/page.tsx
@@ -27,7 +27,7 @@ export default function Requests() {
     <div className="pt-5">
       <p className="pl-5 text-lg">Request & Special Orders</p>
       <div className="mt-6 flex flex-col gap-10 xl:flex-row xl:gap-5 xl:px-5 xl:pb-6">
-        <div className="grid gap-5 px-5 xl:px-0">
+        <div className="flex flex-col gap-5 px-5 xl:px-0">
           {/* Create Request */}
           <div className="border-white-dark rounded-xl rounded-tl-none border p-4">
             <p>Create Request</p>
