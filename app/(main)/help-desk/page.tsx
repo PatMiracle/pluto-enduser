a1b34329ab1f883dd706ca58ec1ed4f948b24e8a
Miracle <pimiracledev@gmail.com>
2025-11-28 09:15:15 +0100

    ui

--- app/(main)/help-desk/page.tsx
+++ app/(main)/help-desk/page.tsx
@@ -18,7 +18,7 @@ export default function HelpDesk() {
   return (
     <div className="px-5">
       <p className="text-lg">Help Desk</p>
-      <div className="flex flex-row gap-5 pt-5">
+      <div className="flex flex-row gap-4 pt-5">
         <Button
           onClick={() => setActiveTab("query-ticket")}
           className={cn(
@@ -39,7 +39,7 @@ export default function HelpDesk() {
           My Tickets
         </Button>
       </div>
-      <div className="py-5">
+      <div className="py-6">
         {activeTab == "query-ticket" ? (
           <QueryTickets />
         ) : (
