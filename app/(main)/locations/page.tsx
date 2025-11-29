02c66c624de8049799ab90cefc0f47bfc6557e21
Miracle <pimiracledev@gmail.com>
2025-11-29 01:39:03 +0100

    wip: added footer to locations

--- app/(main)/locations/page.tsx
+++ app/(main)/locations/page.tsx
@@ -5,13 +5,17 @@ import {
   LocationCard,
   LocationCardSkeleton,
 } from "./location-card";
+import Footer from "@/components/footer";
+import { useState } from "react";
 
 export default function Locations() {
+  const [activeTab, setActiveTab] = useState<"pickup" | "dropoff">("pickup");
+
   const { data: locations } = useClientLocations();
 
   return (
-    <div className="">
-      <div className="grid max-w-3xl gap-5 p-5 md:grid-cols-2">
+    <div className="gap-5 pt-5 xl:flex xl:w-11/12 xl:justify-between">
+      <div className="grid max-w-[790px] shrink-0 gap-5 px-5 pb-5 md:grid-cols-2 xl:flex-1">
         {locations?.data && locations.data.length < 4 && <AddLocation />}
         {locations
           ? locations.data.map((v) => (
@@ -21,6 +25,7 @@ export default function Locations() {
               return <LocationCardSkeleton key={idx} />;
             })}
       </div>
+      <Footer />
     </div>
   );
 }
