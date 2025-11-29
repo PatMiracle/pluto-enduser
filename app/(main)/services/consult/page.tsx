43728e52fb6416b540b6abb33da7384fd3a6a1b9
Miracle <pimiracledev@gmail.com>
2025-11-29 10:58:06 +0100

    wip: consult

--- app/(main)/services/consult/page.tsx
+++ app/(main)/services/consult/page.tsx
@@ -3,12 +3,24 @@
 import { DataTable } from "@/components/data-table";
 import { useConsultation } from "@/services/consult";
 import { consultColumns } from "./consultColumns";
+import { Button } from "@/components/ui/button";
 
 const Consult = () => {
   const { data } = useConsultation({ pageSize: 9 });
   return (
-    <div className="px-5">
-      <p>Consult With Us!</p>
+    <div className="grid gap-4 px-5">
+      <p className="text-lg">Consult With Us!</p>
+
+      <div className="border-white-dark rounded-xl rounded-tl-none border p-4">
+        <p>Need Expert Advice?</p>
+        <div className="text-white-darker mt-2 text-sm">
+          <p>
+            Submit a consultation request and our team will get back to you.
+          </p>
+
+          <Button className="mt-2 max-w-[136px]">Create Now</Button>
+        </div>
+      </div>
 
       <DataTable columns={consultColumns} data={data?.data} />
     </div>
