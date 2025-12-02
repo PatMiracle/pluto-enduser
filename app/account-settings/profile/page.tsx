cd5533a9a402cb29c7fecab59a7627a9c76b7cc8
Miracle <pimiracledev@gmail.com>
2025-12-02 14:22:27 +0100

    wip profile

--- app/account-settings/profile/page.tsx
+++ app/account-settings/profile/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
+
+import { MdEdit } from "react-icons/md";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="max-w-4xl px-5">
+      <div className="flex justify-between">
+        <p className="text-lg">Profile</p>
+        <button className="text-green-normal text-2xl">
+          <MdEdit />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
